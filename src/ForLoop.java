ba0d5358b2a19885846333023006047fc02ce76e
seniu99 <seniu99@gmail.com>
2022-07-23 13:03:19 +0200

    testowanie petla kotek

---
+++ src/ForLoop.java
@@ -0,0 +1,13 @@
+public class ForLoop {
+    public static void main(String[] args) {
+        // komentarz
+        for (int i = 0; i <= 10; i++) {
+            System.out.println(i);
+            if (i == 5) {
+                System.out.println("kotek");
+            }
+        }
+
+
+    }
+}
