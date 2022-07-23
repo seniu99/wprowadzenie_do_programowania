fc817dd666b2b79606fc64d1b76c4e94105770f4
seniu99 <seniu99@gmail.com>
2022-07-23 11:48:17 +0200

    Dodałem Skaner

---
+++ src/TextFromUser.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class TextFromUser {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String userInput = scanner.nextLine();
+        System.out.println("Witaj "+ userInput);
+    }
+}
